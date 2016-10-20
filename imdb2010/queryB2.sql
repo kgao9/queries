2ee044b61f6558e8d11db0a7962daa4c7af7c456
Young <ylee546@wisc.edu>
2016-10-20 08:46:24 -0500

    Wrote queryB2.sql

---
+++ imdb2010/queryB2.sql
@@ -0,0 +1,47 @@
+/*
+CREATE TABLE actor(
+  id int PRIMARY KEY,
+  fname varchar(30),
+  lname varchar(30),
+  gender char(1));
+CREATE TABLE movie(
+  id int PRIMARY KEY,
+  name varchar(150),
+  year int);
+CREATE TABLE directors(
+  id int PRIMARY KEY,
+  fname varchar(30),
+  lname varchar(30));
+CREATE TABLE genre(
+  mid int,
+  genre varchar(50));
+CREATE TABLE casts(
+  pid int REFERENCES actor,
+  mid int REFERENCES movie,
+  role varchar(50));
+CREATE TABLE movie_directors(
+  did int REFERENCES directors,
+  mid int REFERENCES movie);
+*/
+
+.headers on
+.mode column
+.read create-imdb.sql
+
+CREATE UNIQUE INDEX movieid ON movie(id);
+CREATE UNIQUE INDEX actorid ON actor(id);
+CREATE UNIQUE INDEX directorsid ON directors(id);
+
+CREATE INDEX castsmid ON casts(mid);
+CREATE INDEX castspid ON casts(pid);
+
+CREATE INDEX movieyear ON movie(year);
+
+CREATE INDEX moviedirectorsmid ON movie_directors(mid);
+CREATE INDEX moviedirectordid ON movie_directors(did);
+
+SELECT DISTINCT  a.fname, a.lname 
+FROM movie m, casts c, movie_directors md, actor a, directors d 
+WHERE m.id = c.mid AND m.id = md.mid AND m.year = 2010 AND a.id = c.pid AND d.id = md.did AND trim(a.fname) = trim(d.fname) AND trim(a.lname) = trim(d.lname);
+
+
